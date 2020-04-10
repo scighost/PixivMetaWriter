59e54d1a53d2178009078ce056113f2043742175
Scighost <scighost@outlook.com>
2020-04-10 14:37:14 +0800

    添加并行操作

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using PixivMetaWriter;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -16,48 +17,55 @@ namespace ConsoleApp1
             Console.CursorVisible = false;//隐藏光标，好看！
             List<FileInfo> files = FileOperation.GetMatchFiles(dirPath);//获取匹配条件的文件列表
             Console.WriteLine("匹配到符合条件的文件{0}个，开始写入信息……", files.Count);
+            int progressNum = 0;//任务进度
+            int successNum = 0;//成功数
 
-            foreach (FileInfo file in files)
+            Parallel.ForEach(files, (file) =>
             {
-                //显示进度
-                Console.SetCursorPosition(0, Console.CursorTop);//光标位置
-                Console.ForegroundColor = ConsoleColor.DarkBlue;//文字颜色
-                Console.Write(" {0} / {1}", files.IndexOf(file) + 1, files.Count);
-
                 Match match = Regex.Match(file.Name, "[0-9]+_p[0-9]+");//匹配 pid_page
                 string pid_page = match.Value;
                 try
                 {
                     PixivInfo pixivInfo = new PixivInfo(pid_page);//获取作品信息
                     FileOperation.ChangeInfo(file, pixivInfo);//文件操作
-                    //FileOperation.PrintInfo(pixivInfo);
+                    successNum++;//成功
                 }
                 catch (Exception ex)
                 {
-                    
+
                     Directory.CreateDirectory(file.DirectoryName + "\\failure");//创建"\failure"文件夹
                     file.CopyTo(file.DirectoryName + "\\failure\\" + file.Name, true);//写入失败的文件复制到"\failure"下
-                    if (ex.HResult== -2146233079)
+                    if (ex.HResult == -2146233079)
                     {
-                        file.CopyTo(file.DirectoryName + $"\\[!404] [{pid_page}]" + file.Extension,true);//404
+                        file.CopyTo(file.DirectoryName + $"\\[!404] [{pid_page}]" + file.Extension, true);//404
                     }
-                    continue;
                 }
-            }
+                progressNum++;
+                PrintProgress(progressNum, files.Count);
+            });
 
             Console.SetCursorPosition(0, Console.CursorTop);
             Console.ForegroundColor = ConsoleColor.DarkBlue;
-            if (Directory.Exists(dirPath + "\\failure"))
-                Console.WriteLine("任务完成，失败文件已复制到\"failure\"文件夹下，任意键退出");
+            if ((files.Count-successNum)==0)
+                Console.WriteLine("任务完成，任意键退出");
             else
-                Console.WriteLine("任务完成，任意键退出");           
+                Console.WriteLine("失败文件{0}个已复制到\"failure\"文件夹下，任意键退出",files.Count-successNum);
             Console.ReadKey();
+        }
+
+        private static void PrintProgress(int progressNum, int count)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);//光标位置
+            Console.ForegroundColor = ConsoleColor.DarkBlue;//文字颜色
+            Console.Write(new string(' ', $" {progressNum} / {count}".Length * 3));//空白字符
+            Console.SetCursorPosition(0, Console.CursorTop);//返回行首
+            Console.Write(" {0} / {1}", progressNum, count);//显示进度
         }
 
         //程序介绍
         private static void PrintIntro()
         {
-            string intro = "PixivMetaWriter\nVersion 0.1 (又不是不能用版)   Author:Scighost\n\n";
+            string intro = "PixivMetaWriter\nVersion 0.1.1 (又不是不能用版v2)   Author:Scighost\n\n";
             //intro += "本程序功能简陋，限制颇高，单线操作，等待难熬，源码难读，修改不好，如何评价？粗制滥造！\n\n";
             intro += "文件名必须以“pid_page”命名，如“44873217_p0.jpg”；只支持jpg和png格式；";
             //intro += "程序将写入以下元数据：{标题(作品名)，作者(画师名)，日期时间(上传时间)，关键字(作品标签)，备注(作品介绍)}，作品标签包含日文原文和中文翻译；";
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PixivMetaWriter")]
 [assembly: AssemblyDescription("A tool for writing metadata from pixiv information especially tags.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Scighost")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PixivMetaWriter")]
 [assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
